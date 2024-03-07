1be40b1e054f7b931f76bac01af08c0a88ba5a11
Ankur Semle <semleankur@gmail.com>
2024-03-07 23:47:57 +0530

    stopwatch cp

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from "react"
 
 
 const CountdownTimer = () => {
+
+  const [time, setTime] = useState(5400);
+  const [isActive, setIsActive] = useState(false);
+
+  const handleStart = () => {
+    setTime(5400);
+    setIsActive(true);
+  }
+
+  const handlePauseResume = () => {
+    setIsActive((prev) => !prev);
+  }
+
+  
+  const handleStop = () => {
+    setTime(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval: any
+    if(isActive){
+      interval = setInterval(() => {
+        setTime((time) => {
+          if(time > 0) {
+            return time - 1;
+          } else {
+            setIsActive(false);
+            return 0;
+          }
+        })
+      },1000)
+    }
+
+    return () => clearInterval(interval);
+  },[])
+    
+
+
   return (
-    <div>CountdownTimer</div>
+    <div>
+      <h1>Stopwatch</h1>
+      <h1>{time}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handlePauseResume}>{isActive ? 'Pause' : 'Resume'}</button>
+      <button onClick={handleStop}>Stop</button>
+
+    </div>
   )
 }
 
