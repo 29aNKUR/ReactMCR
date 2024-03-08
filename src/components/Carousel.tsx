714370fccea038fc0deb6c33de1d909f87963b1c
Ankur Semle <semleankur@gmail.com>
2024-03-08 22:49:46 +0530

    added images in carousel comp

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,9 @@
 
 const Carousel = () => {
+
+  const data = ["https://images.pexels.com/photos/719396/pexels-photo-719396.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://i.pinimg.com/236x/ce/00/c2/ce00c26ab211f6be2da9fa2c4121713b.jpg","https://wallpapers.com/images/featured/hd-a5u9zq0a0ymy2dug.jpg", "https://e0.pxfuel.com/wallpapers/781/241/desktop-wallpaper-nature-beautiful-natural-nature-thumbnail.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRFUR6CMpA6Dqxx-SEaYdTUUSwEBWBJgSCqA&usqp=CAU","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMXwXcwzjv6l6kvmR1RLZQFMW2w3fSHxlJ4Q&usqp=CAU"];
+
+
   return (
     <div>Carousel</div>
   )
